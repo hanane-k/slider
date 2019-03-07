4708875b3f40c81d0612f9b2019691bdaecd3625
hanane <nam.hana@yahoo.fr>
2019-03-07 10:15:41 +0100

    simplifier le code du slider

--- js/main.js
+++ js/main.js
@@ -5,17 +5,18 @@ var src = [
     "img/plage2.jpg",
     "img/plage3.jpg"
 ]
-// j'initialise une variable pour sotcker mon tour de slider
+// initialiser une variable pour sotcker le tour de slider
 var i = 0;
-// Je récupère toutes mes images dans une variable
-var img = document.getElementById("img");
 // création d'une fonction qui recupere les images à un interval de 3s
 setInterval(function() {
-    document.getElementById("imgI").src = src[i];
-    // afficher les images suivantes dans le tableau l'une à la suite de l'autre
+    // récupération de toutes les images et les lier avec leurs sources
+    imgI = document.getElementById("imgI").src = src[i];
+    // afficher les images suivantes dans le tableau l'une à la suite de l'autre progressivement
     i++;
     // si la derniere image est affichée il revient au debut
     if(i == 3) i = 0;
     // définir le temps de changement des images
 }, 3000);
-// console.log(img);
+
+
+console.log(imgI);
