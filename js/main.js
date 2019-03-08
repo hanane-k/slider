9d07fef74fee10e1da653c6baa41f4d5777a4f53
hanane <nam.hana@yahoo.fr>
2019-03-08 16:08:59 +0100

    btn2 ok diminuer le code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// variable
 // déclarer la variable src
 var src = ["img/plage1.jpg", "img/plage2.jpg", "img/plage3.jpg"];
 // initialiser une variable pour sotcker le tour de slider
@@ -6,8 +5,7 @@ var i = 0;
 // récupération de la premiere image et la lier avec sa source
 imgI = document.getElementById("imgI");
 imgI.src = src[i];
-// création d'une fonction qui recupere les images à un interval de 3s
-var interval  = setInterval(function() {
+function inter() {
     // si la derniere image est affichée il revient au debut
     if(i == src.length - 1) {
         i = 0
@@ -17,41 +15,32 @@ var interval  = setInterval(function() {
     }
     // afficher les images suivantes dans le tableau l'une à la suite de l'autre progressivement
     imgI.src = src[i];
-    // définir le temps de changement des images
-}, 3000);
+}
+// création d'une fonction qui recupere les images à un interval de 3s
+var interval  = setInterval(function() { inter() }, 3000);
 //bouton précédent
 var btn1 = document.getElementById("previous");
-// btn1.addEventListener("click", previous());
 function previous() {
     clearInterval(interval);
-    // alert("hi");
     if (i > 0) {
         i-=1;
     }
+    else {
+        i = src.length - 1;
+    }
     imgI.src = src[i];
-    interval = setInterval(function() {
-        // si la derniere image est affichée il revient au debut
-        if(i == src.length - 1) {
-            i = 0
-        }
-        else{
-            i++;
-        }
-        // afficher les images suivantes dans le tableau l'une à la suite de l'autre progressivement
-        imgI.src = src[i];
-        // définir le temps de changement des images
-    }, 3000);
+    interval = setInterval(function() { inter() }, 3000);
 };
-
-    // if (i==0) {
-    //     i == src.length};
-    // }
-//bouton suivant
-// btn2 = document.getElementById("next");
-// btn2.addEventListener("click", next());
-// function next() {
-//     if (i!== src.length && i <= src.length) {i++;}
-//     imgI.src = src[i];
-//     clearInterval(imgI);
-// };
-    
+// bouton suivant
+var btn2 = document.getElementById("next");
+function next() {
+    clearInterval(interval);
+    if (i < src.length -1) {
+        i+=1;
+    }
+    else {
+        i = 0;
+    }
+    imgI.src = src[i];
+    interval = setInterval(function() { inter() }, 3000);
+};
