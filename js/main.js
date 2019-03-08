744f855002c1a9a1863de95a55e316f4c76253cb
hanane <nam.hana@yahoo.fr>
2019-03-08 12:36:51 +0100

    regler le probleme de l'image1 du slider et btn1 ok

--- js/main.js
+++ js/main.js
@@ -3,38 +3,55 @@
 var src = ["img/plage1.jpg", "img/plage2.jpg", "img/plage3.jpg"];
 // initialiser une variable pour sotcker le tour de slider
 var i = 0;
-    // récupération de la premiere image et la lier avec sa source
-    imgI = document.getElementById("imgI");
-    imgI.src = src[0];
-
+// récupération de la premiere image et la lier avec sa source
+imgI = document.getElementById("imgI");
+imgI.src = src[i];
 // création d'une fonction qui recupere les images à un interval de 3s
-setInterval(function() {
-    // récupération de toutes les images et les lier avec leurs sources
-    imgI = document.getElementById("imgI");
-    imgI.src = src[i];
-    // afficher les images suivantes dans le tableau l'une à la suite de l'autre progressivement
-    i++;
+var interval  = setInterval(function() {
     // si la derniere image est affichée il revient au debut
-    if(i == src.length) {i = 0};
+    if(i == src.length - 1) {
+        i = 0
+    }
+    else{
+        i++;
+    }
+    // afficher les images suivantes dans le tableau l'une à la suite de l'autre progressivement
+    imgI.src = src[i];
     // définir le temps de changement des images
 }, 3000);
 //bouton précédent
 var btn1 = document.getElementById("previous");
-btn1.onclick = function () {
-    if (i >= src.length - 1) {i--;}
+// btn1.addEventListener("click", previous());
+function previous() {
+    clearInterval(interval);
+    // alert("hi");
+    if (i > 0) {
+        i-=1;
+    }
     imgI.src = src[i];
-    clearInterval(imgI);
+    interval = setInterval(function() {
+        // si la derniere image est affichée il revient au debut
+        if(i == src.length - 1) {
+            i = 0
+        }
+        else{
+            i++;
+        }
+        // afficher les images suivantes dans le tableau l'une à la suite de l'autre progressivement
+        imgI.src = src[i];
+        // définir le temps de changement des images
+    }, 3000);
 };
 
     // if (i==0) {
     //     i == src.length};
     // }
 //bouton suivant
-btn2 = document.getElementById("next");
-btn2.onclick = function () {
-    if (i!== src.length && i <= src.length) {i++;}
-    imgI.src = src[n];
-    clearInterval(imgI);
-};
-// console.log(btn1);
+// btn2 = document.getElementById("next");
+// btn2.addEventListener("click", next());
+// function next() {
+//     if (i!== src.length && i <= src.length) {i++;}
+//     imgI.src = src[i];
+//     clearInterval(imgI);
+// };
     
